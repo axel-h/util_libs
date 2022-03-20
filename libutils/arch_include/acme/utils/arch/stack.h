0b7b620f0c290ea9eada257168c42c532da28f2a
Axel Heider <axelheider@gmx.de>
2022-03-20 15:17:41 +0100

    wip add new architecture "acme"

---
+++ libutils/arch_include/acme/utils/arch/stack.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2022, Axel Heider <axelheider@gmx.de>
+ * Copyright 2018, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+/* Number of bytes to which the stack pointer must be aligned when
+ * executing a call instruction
+ */
+#define STACK_CALL_ALIGNMENT  16
